8338074306c8c958834063edf4cc3cf4048b8f81
Gavin Lin <avidoggy@GavindeMacBook-Pro.local>
2021-04-13 01:38:58 +0800

    Add green etl

--- glue/nyc-tlc-green.py
+++ glue/nyc-tlc-green.py
@@ -1,26 +1,127 @@
-from pyspark.sql.types import (BooleanType, DateType, DoubleType, FloatType,
-                               IntegerType, StringType, StructType)
+# native python
+import sys
+import itertools
+
+# AWS GLUE
+from awsglue.context import GlueContext
+from awsglue.dynamicframe import DynamicFrame
+from awsglue.job import Job
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+
+# pyspark
+from pyspark.context import SparkContext
+from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql.utils import AnalysisException
+from pyspark.sql.functions import col, lit
+from pyspark.sql.types import (IntegerType, DoubleType, BooleanType, FloatType, StructField,
+                               StructType, TimestampType)
+
+args = getResolvedOptions(
+    sys.argv, [
+        'JOB_NAME',
+    ]
+)
+
+glueContext = GlueContext(SparkContext.getOrCreate())
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+spark = glueContext.spark_session
+# spark = SparkSession.builder \
+#     .master("local") \
+#     .appName("etl") \
+#     .config("spark.driver.memory", "12g") \
+#     .getOrCreate()
+
 '''
 https://www1.nyc.gov/assets/tlc/downloads/pdf/data_dictionary_trip_records_green.pdf
 '''
-green_schema = StructType()\
-    .add("vendor_name", IntegerType(), True)\
-    .add("lpep_pickup_datetime", DateType(), True)\
-    .add("lpep_dropoff_datetime", DateType(), True)\
-    .add("store_and_fwd_flag", BooleanType(), True)\
-    .add("ratecodeid", IntegerType(), True)\
-    .add("pickup_longitude", DoubleType(), True)\
-    .add("pickup_latitude", DoubleType(), True)\
-    .add("dropoff_longitude", DoubleType(), True)\
-    .add("dropoff_latitude", DoubleType(), True)\
-    .add("passenger_count", IntegerType(), True)\
-    .add("trip_distance", FloatType(), True)\
-    .add("fare_amount", FloatType(), True)\
-    .add("extra", FloatType(), True)\
-    .add("mta_tax", FloatType(), True)\
-    .add("tip_amount", FloatType(), True)\
-    .add("tolls_amount", FloatType(), True)\
-    .add("ehail_fee", FloatType(), True)\
-    .add("total_amount", FloatType(), True)\
-    .add("payment_type", IntegerType(), True)\
-    .add("trip_type_#0", StringType(), True)
+rename = {
+    "vendorid": "vendor_name",
+    "ratecodeid": "ratecode_id",
+    "pulocationid": "pickup_location_id",
+    "dolocationid": "dropoff_location_id",
+}
+green_schema = StructType([
+    StructField("vendor_name", IntegerType(), True),
+    StructField("lpep_pickup_datetime", TimestampType(), True),
+    StructField("lpep_dropoff_datetime", TimestampType(), True),
+    StructField("store_and_fwd_flag", BooleanType(), True),
+    StructField("ratecode_id", IntegerType(), True),
+    StructField("pickup_longitude", DoubleType(), True),
+    StructField("pickup_latitude", DoubleType(), True),
+    StructField("dropoff_longitude", DoubleType(), True),
+    StructField("dropoff_latitude", DoubleType(), True),
+    StructField("pickup_location_id", IntegerType(), True),
+    StructField("dropoff_location_id", IntegerType(), True),
+    StructField("passenger_count", IntegerType(), True),
+    StructField("trip_distance", FloatType(), True),
+    StructField("fare_amount", FloatType(), True),
+    StructField("extra", FloatType(), True),
+    StructField("mta_tax", FloatType(), True),
+    StructField("tip_amount", FloatType(), True),
+    StructField("tolls_amount", FloatType(), True),
+    StructField("ehail_fee", FloatType(), True),
+    StructField("improvement_surcharge", FloatType(), True),
+    StructField("congestion_surcharge", FloatType(), True),
+    StructField("total_amount", FloatType(), True),
+    StructField("payment_type", IntegerType(), True),
+    StructField("trip_type", IntegerType(), True),
+])
+
+dataset_name = 'green'
+source_path = 's3a://nyc-tlc/trip data/{dataset}_tripdata_{year}-{month:02}.csv'
+destination_path = 's3a://gavin-data-lake/nyc-tlc/trip-data/{dataset}/'
+
+spark.createDataFrame(spark.sparkContext.emptyRDD(), green_schema).write.parquet(
+    path=destination_path.format(dataset=dataset_name),
+    mode="overwrite",
+    # partitionBy=["year", "month"],
+    compression="snappy"
+)
+
+years = range(2013, 2021)
+months = range(1, 13)
+for year, month in itertools.product(years, months):
+    print(source_path.format(
+        dataset=dataset_name,
+        year=year, month=month
+    ))
+    try:
+        raw = spark.read.csv(
+            path=source_path.format(
+                dataset=dataset_name,
+                year=year, month=month
+            ),
+            header=True, enforceSchema=False,
+            inferSchema=True, samplingRatio=0.01,
+            ignoreTrailingWhiteSpace=True,
+        )
+        for column in raw.columns:
+            new_col = column.lower()
+            new_col = rename[new_col] if new_col in rename else new_col
+            raw = raw.withColumnRenamed(column, new_col)
+    except AnalysisException as e:
+        print('AnalysisException', e)
+        continue
+    # raw.printSchema()
+    df = raw
+    for field in green_schema.fields:
+        if field.name in df.columns:
+            df = df.withColumn(field.name, col(field.name).astype(field.dataType))
+        else:
+            df = df.withColumn(field.name, lit(None).astype(field.dataType))
+    df = df.select(green_schema.fieldNames())\
+        .withColumn('year', lit(year).astype(IntegerType()))\
+        .withColumn('month', lit(month).astype(IntegerType()))
+    # df.printSchema()
+    # df.show(vertical=True, n=5)
+    df.write.parquet(
+        path=destination_path.format(dataset=dataset_name),
+        mode="append",
+        partitionBy=["year", "month"],
+        compression="snappy"
+    )
+
+job.commit()
---
+++ s3/nyc-tlc-green.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2013-08.csv ../data/green_tripdata_2013-08.csv
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2013-11.csv ../data/green_tripdata_2013-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2014-03.csv ../data/green_tripdata_2014-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2014-11.csv ../data/green_tripdata_2014-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2015-03.csv ../data/green_tripdata_2015-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2015-11.csv ../data/green_tripdata_2015-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2016-03.csv ../data/green_tripdata_2016-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2016-04.csv ../data/green_tripdata_2016-04.csv
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2016-05.csv ../data/green_tripdata_2016-05.csv
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2016-06.csv ../data/green_tripdata_2016-06.csv
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2016-07.csv ../data/green_tripdata_2016-07.csv
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2016-08.csv ../data/green_tripdata_2016-08.csv
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2016-09.csv ../data/green_tripdata_2016-09.csv
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2016-10.csv ../data/green_tripdata_2016-10.csv
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2016-11.csv ../data/green_tripdata_2016-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2017-03.csv ../data/green_tripdata_2017-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2017-11.csv ../data/green_tripdata_2017-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2018-03.csv ../data/green_tripdata_2018-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2018-11.csv ../data/green_tripdata_2018-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2019-03.csv ../data/green_tripdata_2019-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2019-11.csv ../data/green_tripdata_2019-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2020-03.csv ../data/green_tripdata_2020-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/green_tripdata_2020-11.csv ../data/green_tripdata_2020-11.csv
