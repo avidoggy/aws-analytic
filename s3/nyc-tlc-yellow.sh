4eea3497f5a15a842bcf1a9cf25b6d3528bf02ab
Gavin Lin <avidoggy@gmail.com>
2021-04-16 01:25:58 +0800

    Add yellow s3 sync script

---
+++ s3/nyc-tlc-yellow.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2009-08.csv ../data/yellow_tripdata_2009-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2009-11.csv ../data/yellow_tripdata_2009-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2010-08.csv ../data/yellow_tripdata_2010-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2010-11.csv ../data/yellow_tripdata_2010-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2011-08.csv ../data/yellow_tripdata_2011-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2011-11.csv ../data/yellow_tripdata_2011-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2012-08.csv ../data/yellow_tripdata_2012-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2012-11.csv ../data/yellow_tripdata_2012-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2013-08.csv ../data/yellow_tripdata_2013-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2013-11.csv ../data/yellow_tripdata_2013-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2014-03.csv ../data/yellow_tripdata_2014-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2014-11.csv ../data/yellow_tripdata_2014-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2015-03.csv ../data/yellow_tripdata_2015-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2015-11.csv ../data/yellow_tripdata_2015-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2016-03.csv ../data/yellow_tripdata_2016-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2016-11.csv ../data/yellow_tripdata_2016-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2017-03.csv ../data/yellow_tripdata_2017-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2017-11.csv ../data/yellow_tripdata_2017-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2018-03.csv ../data/yellow_tripdata_2018-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2018-11.csv ../data/yellow_tripdata_2018-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2019-03.csv ../data/yellow_tripdata_2019-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2019-11.csv ../data/yellow_tripdata_2019-11.csv
+
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2020-03.csv ../data/yellow_tripdata_2020-03.csv
+aws s3 cp s3://nyc-tlc/trip\ data/yellow_tripdata_2020-11.csv ../data/yellow_tripdata_2020-11.csv
