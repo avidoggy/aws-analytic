9959cc38af5bf66d9ab83240e2d1cfa0bfe227ff
Gavin Lin <avidoggy@GavindeMacBook-Pro.local>
2021-04-16 01:24:19 +0800

    Update script

--- glue/update-job-scripts.sh
+++ glue/update-job-scripts.sh
@@ -3,14 +3,18 @@
 SCRIPT_BUCKET="aws-glue-scripts-727247251057-us-east-1"
 SCRIPT_KEY_PREFIX="gavin.lin"
 
+echo "aws s3 cp nyc-tlc-fhv.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyc-tlc-fhv-job.py"
 aws s3 cp nyc-tlc-fhv.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyc-tlc-fhv-job.py
 # aws glue create-job --cli-input-yaml file://nyc-tlc-fhv-job.yaml
 
+echo "aws s3 cp nyc-tlc-fhvhv.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyc-tlc-fhvhv-job.py"
 aws s3 cp nyc-tlc-fhvhv.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyc-tlc-fhvhv-job.py
 # aws glue create-job --cli-input-yaml file://nyc-tlc-fhvhv-job.yaml
 
+echo "aws s3 cp nyc-tlc-green.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyc-tlc-green-job.py"
 aws s3 cp nyc-tlc-green.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyc-tlc-green-job.py
 # aws glue create-job --cli-input-yaml file://nyc-tlc-green-job.yaml
 
+echo "aws s3 cp nyc-tlc-yellow.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyc-tlc-yellow-job.py"
 aws s3 cp nyc-tlc-yellow.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyc-tlc-yellow-job.py
-# aws glue create-job --cli-input-yaml file://nyc-tlc-yellow-job.yaml
+aws glue create-job --cli-input-yaml file://nyc-tlc-yellow-job.yaml
