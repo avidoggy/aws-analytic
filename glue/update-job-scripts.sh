7ed79a1f1bfa9003d8178cc3a2665170e1eef75a
Gavin Lin <avidoggy@gmail.com>
2021-04-19 13:04:01 +0800

    Rename datamart to datacube

--- glue/update-job-scripts.sh
+++ glue/update-job-scripts.sh
@@ -5,20 +5,20 @@ SCRIPT_KEY_PREFIX="gavin.lin"
 
 echo "aws s3 cp nyc-tlc-fhv.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyc-tlc-fhv-job.py"
 aws s3 cp nyc-tlc-fhv.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyc-tlc-fhv-job.py
-# aws glue create-job --cli-input-yaml file://nyc-tlc-fhv-job.yaml
+aws glue create-job --cli-input-yaml file://nyc-tlc-fhv-job.yaml
 
 echo "aws s3 cp nyc-tlc-fhvhv.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyc-tlc-fhvhv-job.py"
 aws s3 cp nyc-tlc-fhvhv.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyc-tlc-fhvhv-job.py
-# aws glue create-job --cli-input-yaml file://nyc-tlc-fhvhv-job.yaml
+aws glue create-job --cli-input-yaml file://nyc-tlc-fhvhv-job.yaml
 
 echo "aws s3 cp nyc-tlc-green.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyc-tlc-green-job.py"
 aws s3 cp nyc-tlc-green.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyc-tlc-green-job.py
-# aws glue create-job --cli-input-yaml file://nyc-tlc-green-job.yaml
+aws glue create-job --cli-input-yaml file://nyc-tlc-green-job.yaml
 
 echo "aws s3 cp nyc-tlc-yellow.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyc-tlc-yellow-job.py"
 aws s3 cp nyc-tlc-yellow.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyc-tlc-yellow-job.py
 aws glue create-job --cli-input-yaml file://nyc-tlc-yellow-job.yaml
 
-echo "aws s3 cp nyctlc-yellow-datamart.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyctlc-yellow-datamart-job.py"
-aws s3 cp nyctlc-yellow-datamart.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyctlc-yellow-datamart-job.py
-aws glue create-job --cli-input-yaml file://nyctlc-yellow-datamart-job.yaml
+echo "aws s3 cp nyctlc-yellow-datacube.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyctlc-yellow-datacube-job.py"
+aws s3 cp nyctlc-yellow-datacube.py s3://${SCRIPT_BUCKET}/${SCRIPT_KEY_PREFIX}/nyctlc-yellow-datacube-job.py
+aws glue create-job --cli-input-yaml file://nyctlc-yellow-datacube-job.yaml
